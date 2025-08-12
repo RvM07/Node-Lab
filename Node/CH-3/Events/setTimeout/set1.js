a16b7f68aba32ea3e9ea0efc1cc50aefc753014d
Reetesh07 <reeteshmore24@gmail.com>
2025-08-12 12:33:44 +0530

    Update set1.js

--- Node/CH-3/Events/setTimeout/set1.js
+++ Node/CH-3/Events/setTimeout/set1.js
@@ -1,13 +1,11 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
-var f1 = (msg) => {
-    console.log("Message from event (f1):", msg);
-};
+var f1 = ('event', (msg) => {
+    console.log("Message from event:", msg);
+});
 
-var f2 = (msg) => {
-    console.log("Message from event (f2):", msg);
-};
+var f2 =('event', "SBUP");
 
 emitter.on('event', f1);
 emitter.on('event', f2);
@@ -15,4 +13,4 @@ emitter.on('event', f2);
 emitter.emit('event', "Hello, World!");
 
 console.log(emitter.listeners('event'));
-console.log(emitter.listenerCount('event'));
+console.log(emitter.listenersCount('event'));
