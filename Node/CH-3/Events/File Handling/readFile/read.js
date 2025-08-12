168e8d7014b6aaaf567eeb4100dc178c2fbedefa
Reetesh07 <reeteshmore24@gmail.com>
2025-08-12 12:40:12 +0530

    Update read.js

--- Node/CH-3/Events/File Handling/readFile/read.js
+++ Node/CH-3/Events/File Handling/readFile/read.js
@@ -1,2 +1,12 @@
 const fs = require('fs');
 const filePath = './sample.txt';
+const word = 'example';
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+    } else {
+        const occurrences = data.split(word).length - 1;
+        console.log(`The word "${word}" occurs ${occurrences} times in the file.`);
+    }
+});
