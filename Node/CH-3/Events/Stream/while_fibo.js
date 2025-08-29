a614f918d8c6d5595770e0ad7ed52bb68ea903d0
Reetesh07 <reeteshmore24@gmail.com>
2025-08-29 12:48:09 +0530

    Create while_fibo.js

---
+++ Node/CH-3/Events/Stream/while_fibo.js
@@ -0,0 +1,22 @@
+const http = require('http');
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    
+    let n1 = 0;
+    let n2 = 1;
+    let n3;
+    
+    res.write('Fibonacci Series: ' + n1 + ', ' + n2);
+
+    let i = 1;
+    while(i <= 10) {
+        n3 = n1 + n2;
+        res.write(', ' + n3);
+        n1 = n2;
+        n2 = n3;
+        i++;
+    }
+
+    res.write(fibSeries);
+    res.end();
+}).listen(5000);
