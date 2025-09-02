23bbf3ee65cf6ab196f1af1b5e030cca7d91b32d
Reetesh07 <reeteshmore24@gmail.com>
2025-09-02 12:20:31 +0530

    Update pract12.js

--- Node/CH-3/Events/Express/pract12.js
+++ Node/CH-3/Events/Express/pract12.js
@@ -9,4 +9,6 @@ app.get('/About', function (req, res) {
   return res.end('about Page');
 });
 
-app.listen(3000);
+app.listen(3000,()=>{
+    console.log('Server is listening on http://localhost:3000/');
+});
