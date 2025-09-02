afce263bf79eec1fd20cf6ffdaacedbc55de36dd
Reetesh07 <reeteshmore24@gmail.com>
2025-09-02 12:19:08 +0530

    Update display_name.js

--- Node/CH-3/Events/Stream/display_name.js
+++ Node/CH-3/Events/Stream/display_name.js
@@ -3,4 +3,6 @@ var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Class:TY - BCA - C \n Name: Reetesh More');
     res.end();
-}).listen(3000);
+}).listen(3000,()=>{
+    console.log('Server is listening on http://localhost:3000/');
+});
