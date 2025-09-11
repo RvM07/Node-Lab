1bd8f21aabc1500fd54887c01a3e43bc0540b1c3
Reetesh07 <reeteshmore24@gmail.com>
2025-09-11 15:35:12 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,34 +4,52 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 const reqfun = function (req, res, next) {
-    console.log(`Request URL:${req.method} requested at ${req.url}`);
+    console.log(`${req.method} requested at ${req.url}`);
     next();
-}
+};
 
-const formfun = function (res, req, next) {
-    const { name: uname } = req.body.name;
-    if (uname) {
-        req.username = uname;
-        next();
+const formfun = function (req, res, next) {
+    try {
+        const name = req.body.name;
+        if (name) {
+            req.username = name;
+            next();
+        } else {
+            res.status(400).send('Name is required');
+        }
+    } catch (error) {
+        next(error);
     }
-}
+};
 
-const showUser = function (req, res){
-    const username = req.username || 'Guest';
-    res.send(`Hello, ${username}`);
-}
+// User display middleware
+const showUser = function (req, res) {
+    try {
+        const username = req.username || 'Guest';
+        res.send(`Hello, ${username}`);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+};
 
-app.get('/',reqfun,(res,req)=>{
+// Routes
+app.get('/', reqfun, (req, res) => {
     res.send(`
         <form method="POST" action="/process">
             <input type="text" name="name" placeholder="Enter your name"/>
             <button type="submit">Submit</button>
         </form>
     `);
-})
+});
 
 app.post('/process', reqfun, formfun, showUser);
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
