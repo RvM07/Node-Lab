2463cb6993c1f91f38fabfbf74dd08bec5ec7f59
Reetesh07 <reeteshmore24@gmail.com>
2025-09-12 15:56:15 +0530

    Update index.js

--- Ejs/routes/index.js
+++ Ejs/routes/index.js
@@ -4,3 +4,17 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home Page', message: 'Welcome to the Home Page!' });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About Us', message: 'Learn more about us on this page.' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
