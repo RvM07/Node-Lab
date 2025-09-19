a532b17fc94bef1d6c16bd7e09459dad6ef849f5
Reetesh07 <reeteshmore24@gmail.com>
2025-09-19 13:13:48 +0530

    Update script.js

--- Node/CH-5/Practical-13/script.js
+++ Node/CH-5/Practical-13/script.js
@@ -9,14 +9,14 @@ const con = mysql.createConnection({
     database:'Customer'
 });
 
-// con.query('SELECT * FROM Customers',(err,result)=>{
-//     if (err) throw err;
-//     console.log(result);
-// });
-
 con.connect((err)=>{
     if(err) throw err;
     console.log('Connected to database');
+});
+
+con.query('SELECT * FROM Customers',(err,result)=>{
+    if (err) throw err;
+    console.log(result);
 });
 
 app.get('/',(req,res)=>{
@@ -35,6 +35,43 @@ app.get('/customers',(req,res)=>{
     });
 });
 
+app.get('/insert',(req,res)=>{
+    con.query(`INSERT INTO Customers VALUES('Priyanshu','Pune')`,(err,result)=>{
+        if (err) {
+            console.log(err);
+            res.status(500).send('Database query error');
+            return;
+        };
+        console.log('1 row inserted')
+        res.json(result);
+    });
+});
+
+app.get('/delete',(req,res)=>{
+    con.query(`DELETE FROM Customers WHERE name='Priyanshu'`,(err,result)=>{
+        if (err) {
+            console.log(err);
+            res.status(500).send('Database query error');
+            return;
+        };
+        console.log('1 row deleted')
+        res.json(result);
+    });
+});
+
+app.get('/update',(req,res)=>{
+    con.query(`UPDATE Customers SET address="Pune" WHERE name="Vedika"`,(err,result)=>{
+        if (err) {
+            console.log(err);
+            res.status(500).send('Database query error');
+            return;
+        };
+        console.log('1 row updated')
+        res.send(result);
+    });
+});
+
+
 app.listen(3000,()=>{
-    console.log('Server started on port 3000');
+    console.log('Server started on port http://localhost:3000');
 });
